e2515aacf1eec7e4969944af0a1fb82bd8f0a68b
Stan Pak <uriasan1@gmail.com>
2015-01-29 13:14:40 -0500

    portfolio

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="css/bootstrap-3.3.2-dist/css/bootstrap.css" rel="stylesheet">
+    <title>Stan Pak Profile</title>
+</head>
+<body>
+    <div class="row">
+        <div id="subcaption">
+
+        </div>
+        <div id="navbar">
+
+        </div>
+    </div>
+</body>
+</html>
