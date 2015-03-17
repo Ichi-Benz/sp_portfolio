3b3c682d40cad861f56e6ff18e107877d5e24bd7
Stan Pak <uriasan1@gmail.com>
2015-03-17 12:54:39 -0400

    Main CSS

--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 <html>
 <head>
     <link href="css/bootstrap-3.3.2-dist/css/bootstrap.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
     <title>Stan Pak Profile</title>
 </head>
 <body>
     <div class="row">
-        <div id="subcaption">
+        <div class="col-md-12" id="subcaption">
 
         </div>
-        <div id="navbar">
+        <div class="col-md-12"  id="navbar">
+
+        </div>
+    </div>
+    <div class="row">
+        <div id="container" class="container col-md-12">
+
+        </div>
+    </div>
+    <div class="row">
+        <div id="footer" class="panel-footer col-md-12">
 
         </div>
     </div>
